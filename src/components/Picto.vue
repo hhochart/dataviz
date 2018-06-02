61529ee7a73f5b4490d5b94675579f9b247bb880
Herve <h.hochart29@gmail.com>
2018-06-02 12:48:43 +0200

    :ship: loading images now works fine & build

--- src/components/Picto.vue
+++ src/components/Picto.vue
@@ -1,7 +1,7 @@
 <template>
   <figure class="img-container" :style="style">
     <img :src="src" alt="" ref="img">
-    <figcaption class="name" >{{ name }}</figcaption>
+    <figcaption class="name">{{ name }}</figcaption>
   </figure>
 </template>
 
@@ -15,7 +15,7 @@ export default {
     }
   },
   mounted () {
-    this.$nextTick(() => {
+    this.$refs.img.onload = (e) => {
       this.w = this.$refs.img.naturalWidth
       this.h = this.$refs.img.naturalHeight
       if (this.$refs.img.naturalHeight > this.$refs.img.naturalWidth) {
@@ -27,40 +27,40 @@ export default {
         width: `${this.w}px`,
         height: `${this.h}px`
       }
-    })
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  @import '../scss/variables';
+@import '../scss/variables';
 
-  .img-container {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    .name {
-      position: absolute;
-      bottom: -55px;
-      opacity: 0;
-      padding-top: 10px;
-      text-align: center;
-      color: $grisfonce;
-      transition: 0.3s ease-in-out;
-    }
-    &::after {
-      transition: all ease-in-out .3s;
-      background-color: $grisclair;
-      content: '';
-      position: absolute;
-      top: -30px;
-      left: -30px;
-      right: -30px;
-      bottom: -30px;
-      border-radius:100%;
-      z-index: -1;
-    }
+.img-container {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .name {
+    position: absolute;
+    bottom: -55px;
+    opacity: 0;
+    padding-top: 10px;
+    text-align: center;
+    color: $grisfonce;
+    transition: 0.3s ease-in-out;
   }
+  &::after {
+    transition: all ease-in-out 0.3s;
+    background-color: $grisclair;
+    content: '';
+    position: absolute;
+    top: -30px;
+    left: -30px;
+    right: -30px;
+    bottom: -30px;
+    border-radius: 100%;
+    z-index: -1;
+  }
+}
 </style>
